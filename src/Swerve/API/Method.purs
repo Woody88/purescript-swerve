b3ba8b22ae17315c5405960708146baad71a1ba8
Woodson Delhia <woodsondelhia88@gmail.com>
2020-08-01 17:21:53 +0900

    updated bnf and added method module

---
+++ src/Swerve/API/Method.purs
@@ -0,0 +1,25 @@
+module Swerve.API.RequestMethod where 
+
+foreign import kind Verb
+foreign import data CONNECT :: Verb
+foreign import data HEAD    :: Verb
+foreign import data GET     :: Verb
+foreign import data POST    :: Verb
+foreign import data PATCH   :: Verb
+foreign import data PUT     :: Verb
+foreign import data DELETE  :: Verb
+foreign import data OPTIONS :: Verb
+foreign import data TRACE   :: Verb
+
+-- 
+data Method (verb :: Verb) (specs :: # Type)
+
+type Connect specs = Method CONNECT 
+type Head    specs = Method HEAD 
+type Get     specs = Method GET 
+type Pose    specs = Method POST 
+type Patch   specs = Method PATCH 
+type Put     specs = Method Put 
+type Delete  specs = Method Delete 
+type Options specs = Method OPTIONS 
+type Trace   specs = Method TRACE 
