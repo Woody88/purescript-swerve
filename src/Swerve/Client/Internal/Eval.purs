9a3329ae778616843c27994ebe6f21e07bf90799
Woodson Delhia <woodsondelhia88@gmail.com>
2021-01-22 13:30:28 +0900

    pushing changes

--- src/Swerve/Client/Internal/Eval.purs
+++ src/Swerve/Client/Internal/Eval.purs
@@ -2,9 +2,18 @@ module Swerve.Client.Internal.Eval where
 
 import Data.Symbol
 import Data.Variant (Variant)
+import Network.HTTP.Types.Method as H
+import Network.Wai (Application)
 import Swerve.API.Alternative (type (:<|>))
+import Swerve.API.BasicAuth
+import Swerve.API.Capture
+import Swerve.API.Header
+import Swerve.API.QueryParam
+import Swerve.API.Raw
+import Swerve.API.ReqBody
 import Swerve.API.Sub 
 import Swerve.API.Verb (Verb)
+import Type.Equality (class TypeEquals)
 import Unsafe.Coerce (unsafeCoerce)
 
 data Client (m :: Type -> Type) (api :: Type)
@@ -15,11 +24,47 @@ instance _evalClientAlt :: EvalClient (Client m (a :<|> b) ) ((Client m a) :<|> 
 
 instance _evalClientVerb :: EvalClient (Client m (Verb method cts as)) (m (Variant as))
 
+instance _evalClientCapture :: EvalClient (Client m (Capture name a :> api)) (a -> Client m api)
+
+instance _evalClientHeader :: EvalClient (Client m (Header name a :> api)) (a -> Client m api)
+
+instance _evalClientQueryParam :: EvalClient (Client m (QueryParam name a :> api)) (a -> Client m api)
+
+instance _evalClientReqBody :: EvalClient (Client m (ReqBody ctypes a :> api)) (a -> Client m api)
+
+instance _evalClientBasicAuth :: EvalClient (Client m (BasicAuth realm usr :> api)) (BasicAuthData -> Client m api)
+
+instance _evalClientRaw :: EvalClient (Client m Raw) (H.StdMethod -> Client m api)
+
 instance _evalClientPath 
   :: IsSymbol path 
   => EvalClient (Client m (path :> api)) (Client m api)
 
 class EvalHandler (a :: Type) (b :: Type) | a -> b
+
+instance evalHandlerCapture
+  :: EvalHandler api server 
+  => EvalHandler (Capture sym a :> api) (a -> server)
+
+instance evalHandlerHeader
+  :: EvalHandler api server 
+  => EvalHandler (Header sym a :> api) (a -> server)
+
+instance evalHandlerQueryParam
+  :: EvalHandler api server 
+  => EvalHandler (QueryParam sym a :> api) (a -> server)
+
+instance evalHandlerReqBody
+  :: EvalHandler api server 
+  => EvalHandler (ReqBody ctypes a :> api) (a -> server)
+
+instance evalHandlerBasicAuth
+  :: EvalHandler api server 
+  => EvalHandler (BasicAuth realm usr :> api) (BasicAuthData -> server)
+
+instance evalHandlerRaw 
+  :: TypeEquals Application waiApp 
+  => EvalHandler Raw (H.StdMethod -> m waiApp)
 
 instance evalHandlerPath :: 
   ( IsSymbol path 
