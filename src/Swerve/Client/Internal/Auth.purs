a5adf8f093f2b0af1f0146932add1ebf77c1bce4
Woodson Delhia <woodsondelhia88@gmail.com>
2021-01-24 02:30:49 +0900

    Added Auth module for client

---
+++ src/Swerve/Client/Internal/Auth.purs
@@ -0,0 +1,13 @@
+module Swerve.Client.Internal.Auth where 
+
+import Data.Tuple (Tuple)
+import Data.Tuple.Nested ((/\))
+import Network.Wai.Internal (Request)
+
+newtype AuthenticatedRequest a = AuthenticatedRequest (Tuple a (a -> Request -> Request))
+
+unAuthReq :: forall a. AuthenticatedRequest a -> (Tuple a (a -> Request -> Request))
+unAuthReq (AuthenticatedRequest a) = a 
+
+mkAuthenticatedRequest :: forall a. a -> (a -> Request -> Request) -> AuthenticatedRequest a
+mkAuthenticatedRequest val func = AuthenticatedRequest (val /\ func)
