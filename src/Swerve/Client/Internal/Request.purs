549f095effff9ccd522417079e34c0ca1984676c
Woodson Delhia <woodsondelhia88@gmail.com>
2021-01-24 03:09:23 +0900

    Added swerve's Request own request module

---
+++ src/Swerve/Client/Internal/Request.purs
@@ -0,0 +1,32 @@
+module Swerve.Client.Internal.Request where 
+
+import Data.Maybe 
+import Data.Tuple.Nested (type (/\))
+import Network.HTTP.Types as H
+import Network.HTTP.Types.Method as H
+import Network.HTTP.Media 
+
+type RequestRow body path = 
+  ( path        :: path
+  , queryString :: H.Query
+  , body        :: Maybe (body /\ MediaType)
+  , accept      :: Array MediaType
+  , headers     :: H.RequestHeaders
+  , httpVersion :: H.HttpVersion
+  , method      :: H.Method
+  ) 
+
+type Request = { | RequestRow RequestBody String }
+
+data RequestBody = RequestBodyStr String 
+
+defaultRequest :: Request
+defaultRequest = 
+  { path: ""
+  , queryString: []
+  , body: Nothing
+  , accept: []
+  , headers: []
+  , httpVersion: H.http11
+  , method: H.methodGet
+  }
