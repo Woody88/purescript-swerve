433ac119bfe9fa0166db9c6c1441953d467a0b68
Woodson Delhia <woodsondelhia88@gmail.com>
2021-01-24 14:42:32 +0900

    Added Swerve Response type

--- src/Swerve/Client/Internal/Response.purs
+++ src/Swerve/Client/Internal/Response.purs
@@ -8,12 +8,21 @@ import Data.Map as Map
 import Data.Symbol
 import Data.Variant (Variant, inj)
 import Network.HTTP.Media 
-import Network.Wai (Response(..))
+import Network.HTTP.Types as H
 import Swerve.API.ContentType
 import Swerve.API.Status
 import Type.Proxy 
 import Type.RowList 
 import Type.Row as Row
+
+type ResponseRow a =
+  ( status      :: H.Status
+  , headers     :: H.ResponseHeaders
+  , httpVersion :: H.HttpVersion
+  , body        :: a
+  )
+
+type Response = { | ResponseRow String }
 
 class AsResponse ctypes (rl :: RowList Type) (row :: Row Type) | rl -> row where 
   mkResponse :: Proxy ctypes -> Proxy rl -> Response -> Either String (Variant row)
@@ -26,15 +35,13 @@ instance _asResponse ::
   , Row.Cons status a t x
   , IsSymbol status
   ) => AsResponse ctypes (Cons status a Nil) x where 
-  mkResponse ctypesP _ (ResponseString status hdrs str) = case reflectSymbol statusP == show status.code of
+  mkResponse ctypesP _ res = case reflectSymbol statusP == show res.status.code of
     false -> Left "bad"
     true  -> do 
-      let accH         = renderHeader $ map show $ allMime ctypesP  
-      -- let _ = trace accH identity
+      let accH = renderHeader $ map show $ allMime ctypesP  
       bodyDecoder <-  note "could not parse accept" $ canHandleCTypeH ctypesP accH 
-      let (eBody :: Either String a) = bodyDecoder str
+      let (eBody :: Either String a) = bodyDecoder res.body
       (inj statusP) <$> eBody 
     where 
       statusP = SProxy :: _ status  
-      headers = Map.fromFoldable hdrs 
-  mkResponse _ _ _ = Left "invalid response type"
+      headers = Map.fromFoldable res.headers
