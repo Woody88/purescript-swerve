9770d06a9df413c86ac7b115f5a662b84a5db8d5
Woodson Delhia <woodsondelhia88@gmail.com>
2021-01-25 02:32:43 +0900

    Added test for Client

---
+++ test/API.purs
@@ -0,0 +1,67 @@
+module Test.API where 
+
+import Prelude 
+
+import Data.Either
+import Data.Maybe 
+import Effect.Class
+import Effect.Aff
+import Network.Wai 
+import Network.Warp.Settings
+import Network.Warp.Run
+import Node.HTTP as HTTP
+import Node.Net.Server as Net
+import Partial.Unsafe (unsafePartial)
+import Swerve.Client.ClientM
+import Swerve.API
+import Swerve.Server  
+import Swerve.Server as Server
+import Type.Proxy 
+import Unsafe.Coerce
+
+type Person = 
+  { name :: String
+  , age  :: Int
+  } 
+
+type API = Get JSON (Ok Person + Nil)
+
+jimmy :: Person 
+jimmy = { name: "Jimmy", age: 30 }
+
+server :: Server API 
+server = Server.lift root 
+  where 
+    root :: Handler _
+    root = pure $ respond (Proxy :: _ Ok') jimmy
+
+apiApp :: Application
+apiApp = serve (Proxy :: _ API) server
+
+serveStubbedApi :: Settings -> Application -> Aff HTTP.Server
+serveStubbedApi settings app = liftEffect $ runSettings settings app 
+
+stopServer :: HTTP.Server -> Aff Unit
+stopServer = liftEffect <<< flip HTTP.close (pure unit)
+
+type Port = Int
+
+
+getServerPort :: HTTP.Server -> Aff Net.Address
+getServerPort httpserver = do 
+  let netserver = unsafeCoerce httpserver
+  delay $ Milliseconds 200.00  -- without this delay the os will not have the time to assign a port.
+  makeAff \done -> do 
+    maddr <- Net.address netserver
+    case maddr of 
+      Nothing -> done <<< Left $ error "Server is not listening, verify that its properly binded to a port."
+      Just addr -> done $ Right $ unsafePartial $ fromLeft $ addr
+    pure nonCanceler
+
+withStubbedApi :: Settings -> Application -> (BaseUrl -> Aff Unit) -> Aff Unit
+withStubbedApi settings app action =
+  bracket (serveStubbedApi settings app)
+          stopServer
+          (\server -> action =<< (mkBaseUrl <<< _.port) <$> getServerPort server) 
+  where 
+    mkBaseUrl p = "http://localhost:" <> show p
---
+++ test/Client/Main.purs
@@ -0,0 +1,48 @@
+module Test.Client.Main where 
+
+import Prelude
+
+import Data.Either
+import Data.Array.Partial as ArrayP
+import Data.Maybe (Maybe(..))
+import Data.Newtype (wrap, unwrap)
+import Data.String.Base64 as Base64
+import Data.Symbol
+import Data.Tuple (Tuple(..))
+import Data.Variant as V 
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Network.HTTP.Types (hAuthorization, ok200, unauthorized401)
+import Network.Wai as Wai
+import Network.Warp.Settings
+import Partial.Unsafe (unsafePartial)
+import Simple.JSON (writeJSON)
+import Swerve.API.Status
+import Swerve.API.BasicAuth (BasicAuthData(..))
+import Swerve.Client.Internal
+import Swerve.Client.Internal as Client
+import Swerve.Client.ClientM
+import Swerve.Server.Internal.BasicAuth (decodeBAHeader)
+import Test.API
+import Test.Spec (Spec, around, describe, it)
+import Test.Spec.Assertions (fail, shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
+import Type.Proxy
+
+main :: Effect Unit
+main = launchAff_ $ runSpec [consoleReporter] spec  
+
+spec :: Spec Unit
+spec = do 
+  let settings = defaultSettings { port = 0 }
+
+  around (withStubbedApi settings apiApp) do
+    describe "basic client" do
+      it "gets Person" $ \baseUrl -> do
+        let getPerson = Client.lift $ client (Proxy :: _ API)
+        let getOkResult = V.default (Left "bad status") # V.on (SProxy :: _ "200") (\(WithStatus _ p) -> Right p)
+        eRes <- runClientM getPerson baseUrl
+
+        (eRes >>= getOkResult) `shouldEqual` (Right jimmy)
+
