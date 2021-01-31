a21b4ed50125bf37dea51aa9307f13277edd8e57
Woodson Delhia <woodsondelhia88@gmail.com>
2021-01-31 20:23:16 +0900

    Added smart functions

--- src/Swerve/Client/Internal/Request.purs
+++ src/Swerve/Client/Internal/Request.purs
@@ -1,13 +1,16 @@
 module Swerve.Client.Internal.Request where 
 
+import Prelude 
+
 import Data.Maybe 
-import Data.Tuple.Nested (type (/\))
+import Data.Tuple.Nested (type (/\), (/\))
 import Network.HTTP.Types as H
 import Network.HTTP.Types.Method as H
 import Network.HTTP.Media 
+import Swerve.API.Header
 
 type RequestRow body path = 
-  ( url        :: path
+  ( url         :: path
   , queryString :: H.Query
   , body        :: Maybe (body /\ MediaType)
   , accept      :: Array MediaType
@@ -30,3 +33,6 @@ defaultRequest =
   , httpVersion: H.http11
   , method: H.methodGet
   }
+
+addHeader :: forall a. ToHeader a => H.HeaderName -> a -> Request -> Request 
+addHeader name val req = req { headers = [(name /\ toHeader val)] <> req.headers }
