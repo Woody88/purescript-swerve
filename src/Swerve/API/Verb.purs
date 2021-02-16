62fb5a8dbb93f6416aac0cdbcd834a4e70ee6e59
Woodson Delhia <woodsondelhia88@gmail.com>
2021-02-17 00:49:26 +0900

    changed verb content type kind

--- src/Swerve/API/Verb.purs
+++ src/Swerve/API/Verb.purs
@@ -3,20 +3,20 @@ module Swerve.API.Verb where
 import Swerve.API.Method (GET', POST', PUT', PATCH', DELETE')
 import Swerve.API.Types (Method', ContentType')
 
-data Verb :: forall k. Method' -> ContentType' -> k -> Type
+data Verb :: forall k. Method' -> Type -> k -> Type
 data Verb m cts as 
 
-type Get :: forall k. ContentType' -> k -> Type
+type Get :: forall k. Type -> k -> Type
 type Get cts a    = Verb GET' cts a
 
-type Post :: forall k. ContentType' -> k -> Type
+type Post :: forall k. Type -> k -> Type
 type Post cts a   = Verb POST' cts a
 
-type Put :: forall k. ContentType' -> k -> Type
+type Put :: forall k. Type -> k -> Type
 type Put cts a    = Verb PUT' cts a
 
-type Patch :: forall k. ContentType' -> k -> Type
+type Patch :: forall k. Type -> k -> Type
 type Patch cts a  = Verb PATCH' cts a 
 
-type Delete :: forall k. ContentType' -> k -> Type
+type Delete :: forall k. Type -> k -> Type
 type Delete cts a = Verb DELETE' cts a
